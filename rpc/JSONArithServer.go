4b773b4b6b6c918cbc4bcb013887249b4fb671ae
MUjib Programmer <mujib.programmer@gmail.com>
2015-11-18 19:26:57 +0700

    add source code to demonstrate rpc on go languange.

---
+++ rpc/JSONArithServer.go
@@ -0,0 +1,82 @@
+// JSONArithServer
+// A version of the ArithServer that uses JSON encoding
+// Using TCP sockets to waiting for TCPArithServer to connect and perform rpc dial/call on port 1234
+//
+// You can execute the JSONArithServer like this:
+//		JSONArithServer 
+
+
+package main
+
+import (
+	"fmt"
+	"net/rpc"
+	"net/rpc/jsonrpc"
+	"os"
+	"net"
+	"errors"
+)
+
+//import ("fmt"; "rpc"; "os"; "net"; "log"; "http")
+type Args struct {
+	A, B int
+}
+
+type Quotient struct {
+	Quo, Rem int
+}
+
+type Arith int
+
+func (t *Arith) Multiply(args *Args, reply *int) error {
+	*reply = args.A * args.B
+	return nil
+}
+
+func (t *Arith) Divide(args *Args, quo *Quotient) error {
+
+	if args.B == 0 {
+		return errors.New("divide by zero")
+	}
+
+	quo.Quo = args.A / args.B
+	quo.Rem = args.A % args.B
+	
+	return nil
+}
+
+func main() {
+	
+	arith := new(Arith)
+	
+	rpc.Register(arith)
+	
+	tcpAddr, err := net.ResolveTCPAddr("tcp", ":1234")
+	checkError(err)
+	
+	listener, err := net.ListenTCP("tcp", tcpAddr)
+	checkError(err)
+	
+	/* This works:
+	rpc.Accept(listener)
+	*/
+	/* and so does this:
+	*/
+	
+	for {
+		conn, err := listener.Accept()
+		
+		if err != nil {
+			continue
+		}
+		
+		jsonrpc.ServeConn(conn)
+	}
+}
+
+func checkError(err error) {
+	if err != nil {
+		fmt.Println("Fatal error ", err.Error())
+		os.Exit(1)
+	}
+}
